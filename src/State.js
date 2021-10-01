b33a5e45f7f565ee798d4e554b995eb018f60762
William Venegas <rvenegas@espol.edu.ec>
2021-10-01 00:39:50 -0500

    Second example using useState Hook

--- src/State.js
+++ src/State.js
@@ -2,18 +2,31 @@ import React, { useState } from "react";
 
 const State = () => {
   const [counter, setCounter] = useState(0);
+  const [inputText, setInputText] = useState("...");
 
   const updateCounter = () => {
     setCounter(counter + 1);
   };
 
+  const updateText = (event) => {
+    const text = event.target.value;
+    setInputText(text)
+  };
+
   return (
     <div className="container-state">
-      <h2 className="title">useState Hook with a Counter</h2>
-      <h4>{counter}</h4>
-      <button className="btn-click sub-title" onClick={updateCounter}>
-        Click me!
-      </button>
+      <div>
+        <h2 className="title">useState Hook with a Counter</h2>
+        <h4>{counter}</h4>
+        <button className="btn-click sub-title" onClick={updateCounter}>
+          Click me!
+        </button>
+      </div>
+      <div>
+        <h2 className="title">useState Hook with a Input text</h2>
+        <input className="input-text" placeholder="enter something..." onChange={updateText} />
+        <h4>{inputText}</h4>
+      </div>
     </div>
   );
 };
