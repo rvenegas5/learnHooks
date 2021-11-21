6e4ace07885c3e1420ede1382e68a2abd2201e88
William Venegas <rvenegas@espol.edu.ec>
2021-11-20 20:48:29 -0500

    Using useReducer hook

--- src/Reducer.js
+++ src/Reducer.js
@@ -1,20 +1,30 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 const Reducer = () => {
-  const [count, setCount] = useState(0);
-  const [showText, setShowText] = useState(false);
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return {count: state.count + 1, showText: state.showText}
+      case "toogleShowText":
+        return {count: state.count, showText: !state.showText }
+      default:
+        return state
+    }  
+  }
+
+  const [state, dispatch] = useReducer(reducer, {count: 0, showText: true});
 
   return (
     <div>
-      <h1>{ count }</h1>
+      <h1>{ state.count }</h1>
       <button onClick={() => {
-        setCount(count + 1)
-        setShowText(!showText)
+        dispatch({type: "INCREMENT"})
+        dispatch({type: "toogleShowText"})
       }}>
         Click Here
       </button>
 
-      {showText && <p>You have {count} clicks</p>}
+      { state.showText && <p>This is a text</p>}
     </div>
   )
 };
